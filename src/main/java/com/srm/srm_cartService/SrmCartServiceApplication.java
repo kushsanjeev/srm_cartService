6a0cf9530072a45fb4ecbf1cab143574235f5638
Sanjeev Kuahwaha <kumarsv01@gmail.com>
2022-12-06 11:50:27 +0530

    configuration and carService setup

---
+++ src/main/java/com/srm/srm_cartService/SrmCartServiceApplication.java
@@ -0,0 +1,13 @@
+package com.srm.srm_cartService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrmCartServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrmCartServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/srm/srm_cartService/SrmCartServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srm.srm_cartService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrmCartServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
